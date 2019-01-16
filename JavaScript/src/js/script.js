5d8b396e879cfbf76acad9104b1c9f1128a93604
Thaylla Christine <thayllachristineo@gmail.com>
2019-01-16 19:44:08 -0200

    Aula 1 - Add JS

---
+++ JavaScript/src/js/script.js
@@ -0,0 +1,9 @@
+const titleChange = document.querySelector(`.head__title`)
+titleChange.textContent = `Aparecida Nutricionista`
+
+const calcIMCPaulo = document.getElementById(``)
+
+
+function calculateIMC(weigth, heigth) {
+    
+}
