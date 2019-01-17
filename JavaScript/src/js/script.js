3d4b440bb5e9bbe8617b34faac7e38f54e256ec8
Thaylla Christine <thayllachristineo@gmail.com>
2019-01-16 23:52:25 -0200

    Aula 3 - Arrays, Loops e estilos

--- JavaScript/src/js/script.js
+++ JavaScript/src/js/script.js
@@ -1,22 +1,30 @@
-const patient = document.querySelector(`#patientPaulo`)
-const weigth = patient.querySelector(`.patient__weight`).textContent
-const heigth = patient.querySelector(`.patient__heigth`).textContent
-const showIMC = patient.querySelector(`.patient__imc`)
+const pat = document.querySelectorAll(`.patient`)
 
-let weigthValid = true
-let heigthValid = true
+for (let i = 0; i < pat.length; i++) {
+    let patient = pat[i]
 
-if (weigth <= 0 || weigth >= 1000) {
-    console.log(`Peso inválido!`)
-    weigthValid = false
-    showIMC.textContent = `Peso inválido!`
-} else if (heigth <= 0 || heigth >= 3.00) {
-    console.log(`Altura inválida!`)
-    heigthValid = false
-    showIMC.textContent = `Altura inválida!`
-}
+    const weigth = patient.querySelector(`.patient__weight`).textContent
+    const heigth = patient.querySelector(`.patient__heigth`).textContent
+    const showIMC = patient.querySelector(`.patient__imc`)
 
-if (weigthValid && heigthValid) {
-    let calcIMC = weigth / (heigth * heigth)
-    showIMC.textContent = calcIMC
+    let weigthValid = true
+    let heigthValid = true
+
+    if (weigth <= 0 || weigth >= 1000) {
+        console.log(`Peso inválido!`)
+        weigthValid = false
+        showIMC.textContent = `Peso inválido!`
+        patient.classList.add(`patient__invalid`)
+    } else if (heigth <= 0 || heigth >= 3.00) {
+        console.log(`Altura inválida!`)
+        heigthValid = false
+        showIMC.textContent = `Altura inválida!`
+        patient.classList.add(`patient__invalid`)
+    }
+
+    if (weigthValid && heigthValid) {
+        let calcIMC = weigth / (heigth * heigth)
+        calcIMC = calcIMC.toFixed(2)
+        showIMC.textContent = calcIMC
+    }
 }
